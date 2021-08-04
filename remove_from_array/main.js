a655848af69bed41bd8e086cfb04cec39a974663
Alik1604 <rudenko.albert.n@gmail.com>
2021-08-04 20:23:29 +0300

    complete

--- remove_from_array/main.js
+++ remove_from_array/main.js
@@ -1,8 +1,14 @@
-let amount = ['1','2','3','4'];
-let user_arg;
+let amount = ['Cat','2','3','4'];
+let user_arg = 1;
 function remover (amount,user_arg){
     user_arg = prompt();
-    amount.splice(user_arg,1) 
-    console.log(amount)
+    if (amount.includes(user_arg) == true){
+        amount.splice(amount.indexOf(user_arg),1) 
+        console.log(amount)
+    }else{
+        console.log('Sorry, incorect argument')
+    }
+    
 }
 remover(amount,user_arg);
+
